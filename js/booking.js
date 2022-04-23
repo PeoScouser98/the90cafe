24f3dbbf02eb3d90377ec1aea479fc56040cd8e3
PeoScouser98 <quanghiep03198@gmail.com>
2022-04-23 18:40:29 +0700

    updated

--- js/booking.js
+++ js/booking.js
@@ -1,66 +1,71 @@
 // get product list from localstorage
 const productInCart = JSON.parse(localStorage.getItem("productInCart"));
-// render product array to html document
 const productList = $("#product-list");
-const renderProduct = (data) => {
-	const output = data
-		.map((item, index) => {
-			item.totalPrice = item.price * item.quantity;
-			// console.log(index);
-			return /*html */ `<div class="product">
-				<div class="font-bold product-name" style="color:var(--primary-1)">${item.name}</div>
-				<div class="product-infor">
-					<div class="price">${item.price}</div>
-					<div>
-						<input type="number" class="quantity border border-gray-200 rounded-lg" min="1" value="${item.quantity}" oninput="calculateSum(this)">
-					</div>
-					<div class="total-price">${item.totalPrice}</div>
-					<div>
-						<button class="remove-btn px-8 py-3" onclick="removeProduct(this,${index})">
-							Xóa
-						</button>
-					</div>
-				</div>
-			</div>`;
-		})
-		.join("");
+const totalAmount = $("#total-amount");
 
-	productList.innerHTML = output;
-};
+/**
+ * RENDER PRODUCT IN CART FROM LOCAL STORAGE
+ */
+function renderProduct(data) {
+	if (Array.isArray(data)) {
+		console.log(typeof data);
+		const products = data
+			.map((item, index) => {
+				item.totalPrice = item.price * item.quantity;
+				return /*html */ `<div class="product">
+					<div class="font-bold product-name" style="color:var(--primary-1)">${item.name}</div>
+					<div class="product-infor">
+						<div class="price">${item.price}</div>
+						<div>
+							<input type="number" class="quantity border border-gray-200 rounded-lg" min="1" value="${item.quantity}" oninput="calculateTotalPrice(this,${index})">
+						</div>
+						<div class="total-price">${item.totalPrice}</div>
+						<div>
+							<button class="remove-btn px-8 py-3" onclick="removeProduct(${index})">
+								Xóa
+							</button>
+						</div>
+					</div>
+				</div>`;
+			})
+			.join("");
+		productList.innerHTML = products;
+		getTotalAmount();
+		if (totalAmount.innerText == 0) showEmpty();
+	}
+}
 renderProduct(productInCart);
 
-// get total price
-const allPrice = [];
-const totalAmount = $("#total-amount");
-const allTotalPrice = $$(".total-price");
-for (const totalPrice of allTotalPrice) {
-	allPrice.push(+totalPrice.innerText);
-}
-const getTotalAmount = () => {
-	totalAmount.innerHTML = allPrice.reduce((currentValue, firstValue) => {
-		return currentValue + firstValue;
-	}, 0);
-};
-getTotalAmount();
-function calculateSum(input) {
-	const price =
-		input.parentElement.parentElement.querySelector(".price").innerHTML;
-	const quantity = input.value;
+/**
+ * CALCULATE PRODUCT'S TOTAL PRICE WHEN CHANGING QUANTITY
+ */
+function calculateTotalPrice(input, index) {
+	const product = productInCart.find((item) => item.id == index);
+	product.totalPrice = product.price * input.value;
 	const totalPrice =
 		input.parentElement.parentElement.querySelector(".total-price");
-	// remove previous total price
-	allPrice.splice(allPrice.indexOf(+totalPrice.innerHTML), 1);
-	totalPrice.innerHTML = +quantity * +price;
-	// push current total price
-	allPrice.push(+totalPrice.innerHTML);
-	console.log(allPrice);
+	totalPrice.innerText = product.totalPrice;
 	getTotalAmount();
 }
-// show empty message
-const showEmpty = () => {
-	if (totalAmount.innerText == 0)
-		productList.innerHTML = /*html */ `<div class="flex flex-col justify-center items-center gap-10 relative top-1/2 -translate-y-1/2">
-			<p class="text-center font-bold" style="font-size:80px; color:red"><i class="bi bi-cart-x"></i></p>
+
+/**
+ * REMOVE PRODUCT FROM CART
+ *  */
+function removeProduct(index) {
+	const deletedProduct = productInCart.find((item) => item.id == index);
+	productInCart.splice(productInCart.indexOf(deletedProduct), 1);
+	localStorage.setItem("productInCart", JSON.stringify(productInCart));
+	renderProduct(productInCart);
+	getTotalAmount();
+	if (totalAmount.innerText == 0) showEmpty();
+}
+
+/**
+ * SHOW EMPTY MESSAGE IF THERE IS NO PRODUCT IN CART
+ *  */
+function showEmpty() {
+	productList.innerHTML = /*html */ `<div class="flex flex-col justify-center items-center gap-10 relative top-1/2 -translate-y-1/2">
+	<p class="text-center font-bold" style="font-size:80px; color:red"><i class="bi bi-cart-x"></i></p>
 			<p class="text-center relative">
 				<a
 					href="https://peoscouser98.github.io/the90cafe/product.html"
@@ -69,23 +74,20 @@ const showEmpty = () => {
 				>
 			</p>
 		</div>`;
-};
-showEmpty();
-// removing item form cart function
-const removeProduct = (button, index) => {
-	const product = button.parentElement.parentElement.parentElement;
-	const totalPrice = product.querySelector(".total-price");
-	allPrice.splice(allPrice.indexOf(+totalPrice.innerHTML), 1);
-	product.remove(product);
-	// remove object inside array from localstorage
-	const filteredElement = productInCart.find((item) => item.id == index);
-	console.log(filteredElement);
-	productInCart.splice(productInCart.indexOf(filteredElement), 1);
-	// console.log(productInCart);
-	localStorage.setItem("productInCart", JSON.stringify(productInCart));
-	getTotalAmount();
-	showEmpty();
-};
+}
+
+/**
+ * CALCULATE TOTAL AMOUNT THAT CLIENT HAVE TO PAY
+ */
+function getTotalAmount() {
+	totalAmount.innerHTML = productInCart.reduce(
+		(previousValue, currentValue) => {
+			return previousValue + currentValue.totalPrice;
+		},
+		0
+	);
+}
+
 // check booking validate
 const name = $("#name");
 const phone = $("#phone");
@@ -93,20 +95,19 @@ const detailAddress = $("#detail-address");
 const district = $("#district");
 
 // check validate form when clicking on submit button
-isRequired([name, phone, detailAddress, district]);
-checkLength(phone, 10);
-isPhoneNumber(phone);
 const submitButton = $("#booking-submit");
-submitButton.onclick = () => {
-	checkNull([name, phone, detailAddress, district]);
-	const allMessage = $$(".error-message");
-	let checkValidate = false;
+submitButton.onclick = function () {
+	isRequired([name, phone, detailAddress, district]);
+	checkLength(phone, 10);
+	isPhoneNumber(phone);
+	const allMessage = $(".error-message");
+	let errorCount = 0;
 	for (const message of allMessage) {
-		if (message.innerText === "") checkValidate = true;
+		if (message.innerText != "") errorCount++;
 	}
 	const totalAmount = $("#total-amount");
-	if (totalAmount.innerText != 0) checkProduct = true;
-	if (checkValidate === true && checkProduct === true) {
+	if (totalAmount.innerText != 0) hasProduct = true;
+	if (errorCount == 0 && hasProduct === true) {
 		swal({
 			title: "Thank you!",
 			text: "Cảm ơn bạn đã đã đặt sản phẩm của chúng tôi !",
